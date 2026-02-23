b940f5e731a635982feacced483fc59a453b2d24
Saloni Kumari <salonisingh0299@gmail.com>
2026-02-23 21:21:14 +0530

    update api url

--- day8/Frontend/src/App.jsx
+++ day8/Frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   console.log("Hello Integration");
 
   function fetchNotes() {
-    axios.get("http://localhost:3000/api/notes")
+    axios.get("https://backend-w6gt.onrender.com/api/notes")
       .then(res => {
         setNotes(res.data.notes)
       })
@@ -26,7 +26,7 @@ function App() {
 
     console.log(title.value, description.value);
 
-    axios.post("http://localhost:3000/api/notes",{
+    axios.post("https://backend-w6gt.onrender.com/api/notes",{
       title:title.value,
       description:description.value
     })
@@ -38,7 +38,7 @@ function App() {
   }
 
   function handleDeleteNote(noteId){
-    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    axios.delete("https://backend-w6gt.onrender.com/api/notes/"+noteId)
     .then(res=>{
       console.log(res.data);
       fetchNotes()
